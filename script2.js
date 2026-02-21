83b6ccf5be11fd82dec2fafb372d41b6580a5ab4
mariahelenadev <mariahelena.pru@gmail.com>
2026-02-20 21:17:59 -0300

    Cadastro Simples

---
+++ script2.js
@@ -0,0 +1,5 @@
+let nome = prompt('Digite seu nome: ');
+let idade = prompt('Digite sua idade: ');
+let cidade = prompt('Digite sua cidade: ');
+
+alert('Seu nome: '+nome+', Sua idade: '+idade+', Sua cidade: '+cidade+'.');
