5e144aeafa4f28388d2f91cf81a5577b6deb8fbb
mariahelenadev <mariahelena.pru@gmail.com>
2026-03-07 16:44:04 -0300

    Add files via upload

--- list.js
+++ list.js
@@ -1,33 +1,31 @@
 let lista = [];
 
-function adicionarItem(){
+    function adicionarItem(){
 
-let item = document.getElementById("item").value;
+    let item = document.getElementById("item").value;
 
-lista.push(item);
+    lista.push(item);
 
-document.getElementById("resultado").innerHTML =
-"Item adicionado!";
+    document.getElementById("resultado").innerHTML = "Item adicionado!";
 
 }
 
 function removerItem(){
 
-let item = document.getElementById("item").value;
+    let item = document.getElementById("item").value;
 
-let posicao = lista.indexOf(item);
+    let posicao = lista.indexOf(item);
 
-if(posicao != -1){
+    if(posicao != -1){
 
-lista.splice(posicao,1);
+    lista.splice(posicao,1);
 
-document.getElementById("resultado").innerHTML =
-"Item removido";
+    document.getElementById("resultado").innerHTML = "Item removido";
 
-}else{
-
-document.getElementById("resultado").innerHTML =
-"Item não encontrado";
+}
+else{
+    
+    document.getElementById("resultado").innerHTML = "Item não encontrado";
 
 }
 
@@ -35,19 +33,17 @@ document.getElementById("resultado").innerHTML =
 
 function pesquisarNome(){
 
-let item = document.getElementById("item").value;
+    let item = document.getElementById("item").value;
 
-let posicao = lista.indexOf(item);
+    let posicao = lista.indexOf(item);
 
-if(posicao != -1){
+    if(posicao != -1){
 
-document.getElementById("resultado").innerHTML =
-"Item encontrado na posição: " + posicao;
+    document.getElementById("resultado").innerHTML = "Item encontrado na posição: " + posicao;
 
 }else{
 
-document.getElementById("resultado").innerHTML =
-"Item não encontrado";
+    document.getElementById("resultado").innerHTML = "Item não encontrado";
 
 }
 
@@ -55,17 +51,15 @@ document.getElementById("resultado").innerHTML =
 
 function pesquisarPosicao(){
 
-let pos = prompt("Digite a posição do item");
+    let pos = prompt("Digite a posição do item");
 
-if(pos >= 0 && pos < lista.length){
+    if(pos >= 0 && pos < lista.length){
 
-document.getElementById("resultado").innerHTML =
-"Item na posição " + pos + ": " + lista[pos];
+    document.getElementById("resultado").innerHTML = "Item na posição " + pos + ": " + lista[pos];
 
-}else{
+    }else{
 
-document.getElementById("resultado").innerHTML =
-"Posição inválida";
+    document.getElementById("resultado").innerHTML = "Posição inválida";
 
 }
 
@@ -73,7 +67,6 @@ document.getElementById("resultado").innerHTML =
 
 function mostrarLista(){
 
-document.getElementById("resultado").innerHTML =
-"Lista: " + lista.join(", ");
+    document.getElementById("resultado").innerHTML = "Lista: " + lista.join(", ");
 
 }
